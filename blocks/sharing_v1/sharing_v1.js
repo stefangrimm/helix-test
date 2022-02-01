862c8affe59dc2fa9da2bebdbe16a7b1405a5845
Stefan Grimm <sgrimm@adobe.com>
2022-02-01 16:57:30 +0100

    Add (basic) sharing component

--- blocks/sharing_v1/sharing_v1.js
+++ blocks/sharing_v1/sharing_v1.js
@@ -1,6 +1,10 @@
 export default function decorate($block) {
   $block.querySelectorAll(':scope a').forEach(($a) => {
     $a.classList.add('pinterest');
+    const $counterSpan = document.createElement('span');
+    $counterSpan.classList.add('pinterest-counter');
+    $counterSpan.appendChild(document.createTextNode('0'));
+    $a.appendChild($counterSpan);
     $a.appendChild(document.createTextNode('Save'));
   });
 }
