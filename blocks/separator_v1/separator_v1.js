31f8c6cc32432484ae403218397de39bd52713c0
Stefan Grimm <sgrimm@adobe.com>
2022-02-01 15:52:30 +0100

    Add separator box/component

--- blocks/separator_v1/separator_v1.js
+++ blocks/separator_v1/separator_v1.js
@@ -1,4 +1,4 @@
 export default function decorate($block) {
-  // hr tags get removed, so let's add it again
-  // $block.appendChild(document.createElement('hr'));
+  // hr markdown gets removed, so let's add it again
+  $block.appendChild(document.createElement('hr'));
 }
